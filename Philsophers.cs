540ceec1e3fe69c9156bd4ad13a4c2040d6803c9
JohanDraaby <37653945+JohanDraaby@users.noreply.github.com>
2019-05-27 08:41:36 +0200

    Add files via upload

---
+++ Philsophers.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiningPhilsophers
+{
+    class Philsophers
+    {
+        public string Name { get; set; }
+        public int LocationAtTable { get; set; }
+        private int tempLocationAtTable { get; set; }
+        private Random Random { get; set; } = new Random();
+
+        public Philsophers(string name, int locationAtTable)
+        {
+            Name = name;
+            LocationAtTable = locationAtTable;
+        }
+
+        public Philsophers()
+        {
+                
+        }
+
+        public int GetForkIndex(int index)
+        {
+            return index % LocationAtTable;
+        }
+
+        public void Work()
+        {
+
+            while (true)
+            {
+                try
+                {
+                    // Attempt to get the left fork..
+                    if (Monitor.TryEnter(Program.Forks[LocationAtTable], 1500))
+                    {
+                        // Write message to screen..
+                        Console.WriteLine("Philosopher {0} har fået venstre gaffel.", LocationAtTable);
+                        Thread.Sleep(1500);
+
+                        // If we got the left fork, try to grab the right fork as well.
+                        if (Monitor.TryEnter(Program.Forks[LocationAtTable +1], 1500))
+                        {
+                            // Write message to screen...
+                            Console.WriteLine("Philosopher {0} Har fået sin højre gaffel.", LocationAtTable);
+                            Thread.Sleep(1500);
+
+                            // Set Philosopher to eat...
+                            Console.BackgroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Philosopher {0} Har sat sig til at spise", LocationAtTable);
+                            Thread.Sleep(Random.Next(1000, 5000));      // The Philosopher eat at random duration.
+                            Console.ResetColor();
+                        }
+                    }
+                    else if (Monitor.TryEnter(Program.Forks[LocationAtTable +1], 1500))
+                    {
+                        // Write message to screen...
+                        Console.WriteLine("Philosopher {0} Har fået sin højre gaffel.", LocationAtTable);
+                        Thread.Sleep(1500);
+
+                        // If it succeded to take first fork try next one...
+                        if (Monitor.TryEnter(Program.Forks[LocationAtTable], 1500))
+                        {
+                            // Write message to screen...
+                            Console.WriteLine("Philosopher {0} Har fået sin venstre gaffel.", LocationAtTable);
+                            Thread.Sleep(1500);
+
+                            // Set philosopher to sleep...
+                            Console.BackgroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Philosopher {0} Har sat sig til at spise.", LocationAtTable);
+                            Thread.Sleep(Random.Next(1000, 5000));
+                            Console.ResetColor();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+                finally
+                {
+                    // Check if fork are taken and lay it back on table...
+                    if (Monitor.IsEntered(Program.Forks[LocationAtTable]))
+                    {
+                        // Lay the fork on table and write message...
+                        Monitor.Exit(Program.Forks[LocationAtTable]);
+                        Console.WriteLine("Philosopher {0} Er færdig med at spise og har lagt sin venstre gaffel.", LocationAtTable);
+                        Thread.Sleep(1500);
+                    }
+                    if (Monitor.IsEntered(Program.Forks[LocationAtTable]))
+                    {
+                        // Lay the fork on table and write message...
+                        Monitor.Exit(Program.Forks[LocationAtTable +1]);
+                        Console.WriteLine("Philosopher {0} Er færdig med at spise og har lagt sin højre gaffel.", LocationAtTable);
+                        Thread.Sleep(1500);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace DiningPhilsophers
+{
+    class Program
+    {
+        // Create a bool array of forks...
+        public static bool[] Forks = new bool[5];       // Contains 5 forks - one for each philosopher..
+        public static Philsophers[] Philsophers { get; set; } = new Philsophers[5];
+        public static Thread[] Threads { get; set; } = new Thread[5];
+
+
+        public static void Main(string[] args)
+        {
+            // Loop for each philosopher to add...
+            for (int i = 0; i < 5; i++)
+            {
+                // Create new philosopher and add to Philosophers array...
+                Philsophers[i] = new Philsophers
+                {
+                    Name = i.ToString(),
+                    LocationAtTable = i
+                    
+                };
+
+                try
+                {
+                    // Create thread for each philosophers work...
+                    Threads[i] = new Thread(Philsophers[i].Work)
+                    {
+                        Name = $"Philosopher {i.ToString()}"
+                    };
+                }
+                catch (Exception ex)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+                
+            }
+
+            // Loop through created threads and start and join them...
+            foreach (var thread in Threads)
+            {
+                thread.Start();     // Start the thread and do work..
+                //thread.Join();      // Join the thread.
+            }
+        }
+    }
+}
